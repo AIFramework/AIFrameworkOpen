d5b8e9a2786d7aa42853c8db95d6932f2af18a65
Zahar <ponimashz@mail.ru>
2022-02-20 00:00:48 +0300

    Статистика

---
+++ AI/Statistics/Distributions/IDistributionWithotParams.cs
@@ -0,0 +1,36 @@
+﻿using AI.DataStructs.Algebraic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Statistics.Distributions
+{
+    public interface IDistributionWithoutParams
+    {
+        /// <summary>
+        /// Probability calculation
+        /// </summary>
+        /// <param name="x">Input</param>
+        double CulcProb(Vector x);
+
+        /// <summary>
+        /// Probability calculation
+        /// </summary>
+        /// <param name="x">Input</param>
+        double CulcProb(double x);
+
+        /// <summary>
+        /// Log probability calculation
+        /// </summary>
+        /// <param name="x">Input</param>
+        double CulcLogProb(double x);
+
+        /// <summary>
+        /// Log probability calculation
+        /// </summary>
+        /// <param name="x">Input</param>
+        double CulcLogProb(Vector x);
+    }
+}
---
+++ AI/Statistics/MixtureModeling/MixtureModel.cs
@@ -0,0 +1,55 @@
+﻿using AI.DataStructs.Algebraic;
+using AI.Statistics.Distributions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Statistics.MixtureModeling
+{
+    public class MixtureModel : IDistributionWithoutParams
+    {
+        public bool IsOneD { get; private set;}
+        Dictionary<string, double>[] _paramDists1D;
+        Dictionary<string, Vector>[] _paramDistsND;
+        Vector _w;
+        IDistribution _perentDistribution;
+
+        public MixtureModel(IDistribution perentDistribution, IEnumerable<Dictionary<string, double>> paramDists, Vector w)
+        {
+            IsOneD = true;
+            _paramDists1D = paramDists.ToArray();
+            _perentDistribution = perentDistribution;
+            _w = w;
+        }
+
+        public MixtureModel(IDistribution perentDistribution, IEnumerable<Dictionary<string, Vector>> paramDists, Vector w)
+        {
+            IsOneD = false;
+            _paramDistsND = paramDists.ToArray();
+            _perentDistribution = perentDistribution;
+            _w = w;
+        }
+
+        public double CulcLogProb(double x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CulcLogProb(Vector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CulcProb(Vector x)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CulcProb(double x)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

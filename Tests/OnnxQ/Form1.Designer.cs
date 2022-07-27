69cd777f7f830e6182656be47c10fe3f46a00352
Zahar A. Ponimash <ponimashz@mail.ru>
2022-07-28 01:50:43 +0300

    Работа с моделями ONNX

---
+++ Tests/OnnxQ/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace OnnxQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.chartVisual1 = new AI.Charts.Control.ChartVisual();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(163, 162);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Стереть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(81, 178);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Распознать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // chartVisual1
+            // 
+            this.chartVisual1.AutoScroll = true;
+            this.chartVisual1.BackColor = System.Drawing.Color.White;
+            this.chartVisual1.ChartName = "График";
+            this.chartVisual1.ForeColor = System.Drawing.Color.Black;
+            this.chartVisual1.IsContextMenu = true;
+            this.chartVisual1.IsLogScale = false;
+            this.chartVisual1.IsMoove = true;
+            this.chartVisual1.IsScale = true;
+            this.chartVisual1.IsShowXY = true;
+            this.chartVisual1.LabelX = "Ось Х";
+            this.chartVisual1.LabelY = "Ось Y";
+            this.chartVisual1.Location = new System.Drawing.Point(169, 0);
+            this.chartVisual1.Name = "chartVisual1";
+            this.chartVisual1.Size = new System.Drawing.Size(727, 314);
+            this.chartVisual1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(-3, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 326);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chartVisual1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Mnist ONNX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private AI.Charts.Control.ChartVisual chartVisual1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tests/OnnxQ/Form1.cs
@@ -0,0 +1,132 @@
+﻿using AI.DataStructs.Algebraic;
+using AI.ONNX.Classifiers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnnxQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Clear();
+            brushDr = new SolidBrush(Color.Black);
+            brushCl = new SolidBrush(Color.White);
+            
+            classifier = new GrayScaleClassifier(@"Onnx mnist\model_q.onnx");
+        }
+
+        Bitmap bitmap;
+        Brush brushDr, brushCl;
+        GrayScaleClassifier classifier;
+
+        void Clear() 
+        {
+            int h = pictureBox1.Height;
+            int w = pictureBox1.Width;
+            bitmap = new Bitmap(w, h);
+            using (Graphics g = Graphics.FromImage(bitmap)) 
+            {
+                g.Clear(Color.White);
+            }
+            
+            pictureBox1.Image = bitmap;
+        }
+
+        Bitmap Crop() 
+        {
+            List<int> xList = new List<int>();
+            List<int> yList = new List<int>();
+
+            int w = bitmap.Width;
+            int h = bitmap.Height;
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    if (bitmap.GetPixel(i, j).R < 70)
+                    {
+                        xList.Add(i);
+                        yList.Add(j);
+                    }
+                }
+            }
+
+            try
+            {
+                int x = xList.Min(), y = yList.Min(), wR = xList.Max() - x, hR = yList.Max() - y;
+                Rectangle rectangle = new Rectangle(x, y, wR, hR);
+
+                return bitmap.Clone(rectangle, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            }
+
+            catch 
+            {
+                return bitmap;
+            }
+        }
+
+        Vector Predict(Bitmap bmp) 
+        {
+            Bitmap bitmapRec = new Bitmap(bmp, 28, 28);
+
+            Matrix m = 1 - AI.ComputerVision.ImageMatrixConverter.BmpToMatr(bitmapRec) / 255;
+            Vector outp = classifier.Classify(m);
+            return outp;
+            
+        }
+
+        // Стереть
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        // Распознать
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = Crop();
+            var v = Predict(bmp);
+            pictureBox1.Image = bmp;
+            label1.Text = $"Это цифра: {v.MaxElementIndex()}";
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Рисование
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int x = e.X;
+            int y = e.Y;
+
+            if (e.Button == MouseButtons.Left|| e.Button == MouseButtons.Right)
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    Brush brush = e.Button == MouseButtons.Left ? brushDr : brushCl;
+                    g.FillEllipse(brush, x - 10, y - 10, 20, 20);
+                }
+
+                pictureBox1.Image = bitmap;
+                chartVisual1.BarBlack(Predict(bitmap));
+            }
+
+            
+
+        }
+    }
+}

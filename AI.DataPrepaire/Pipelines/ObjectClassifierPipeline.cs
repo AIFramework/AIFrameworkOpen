12bd1fecfa86db6ec5bcd8c40878f8c38c85cc0d
Zahar A. Ponimash <ponimashz@mail.ru>
2022-08-24 17:51:44 +0300

    Доработка классификаторов

--- AI.DataPrepaire/Pipelines/ObjectClassifierPipeline.cs
+++ AI.DataPrepaire/Pipelines/ObjectClassifierPipeline.cs
@@ -1,9 +1,12 @@
 ﻿using AI.DataPrepaire.DataNormalizers;
 using AI.DataPrepaire.FeatureExtractors;
+using AI.DataPrepaire.Pipelines.Utils;
 using AI.DataStructs.Algebraic;
+using AI.Extensions;
 using AI.ML.AlgorithmAnalysis;
 using AI.ML.Classifiers;
 using AI.ML.DataSets;
+using AI.Statistics;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -19,6 +22,15 @@ namespace AI.DataPrepaire.Pipelines
     [Serializable]
     public abstract class ObjectClassifierPipeline<T>
     {
+
+        Random random1 = new Random(1);
+
+
+        /// <summary>
+        /// Индекс неизвестного класса
+        /// </summary>
+        public int UnknowClass { get; set; } = -1;
+
         /// <summary>
         /// Извлечение признаков из данных
         /// </summary>
@@ -33,6 +45,11 @@ namespace AI.DataPrepaire.Pipelines
         /// Классификатор
         /// </summary>
         public IClassifier Classifier { get; set; }
+
+        /// <summary>
+        /// Детектор аномальных и/или неподходящих данных
+        /// </summary>
+        public IDetector<T> Detector { get; set; }
 
         /// <summary>
         /// Конвейер обработки данных, классификация объекта
@@ -52,7 +69,6 @@ namespace AI.DataPrepaire.Pipelines
                 (Vector)Normalizer.Transform(
                     Extractor.GetFeatures(input)));
         }
-
 
         /// <summary>
         /// Запуск классификатора
@@ -80,6 +96,44 @@ namespace AI.DataPrepaire.Pipelines
                     Extractor.GetFeatures(input)));
         }
 
+        /// <summary>
+        /// Запуск классификатора (Возвращает векторы распределений)
+        /// </summary>
+        /// <param name="input">Вход</param>
+        public virtual Vector[] ClassifyProb(T[] input)
+        {
+            Vector[] clProbs = new Vector[input.Length];
+
+            for (int i = 0; i < clProbs.Length; i++)
+            {
+                clProbs[i] = ClassifyProb(input[i]);
+            }
+
+            return clProbs;
+        }
+
+        /// <summary>
+        /// Классификация объекта на базе распределения
+        /// </summary>
+        /// <param name="input">Входной объект</param>
+        public virtual int StoсhasticClassify(T input)
+        {
+            return RandomItemSelection<T>.GetIndex(ClassifyProb(input), random1);
+        }
+
+        /// <summary>
+        /// Классификация объекта на базе распределения
+        /// </summary>
+        /// <param name="input">Входной объект</param>
+        public virtual int[] StoсhasticClassify(T[] input)
+        {
+            int[] clsIds = new int[input.Length];
+
+            for (int i = 0; i < clsIds.Length; i++)
+                 clsIds[i] = StoсhasticClassify(input[i]);
+            
+            return clsIds;
+        }
 
         /// <summary>
         /// Обучение ковейера
@@ -88,9 +142,12 @@ namespace AI.DataPrepaire.Pipelines
         /// <param name="labels"></param>
         public virtual void Train(IEnumerable<T> data, IEnumerable<int> labels)
         {
+            //Очистка данных
+            DatasetForClassifier dataSamples = ClearData(data.ToArray(), labels.ToArray());
+            dataSamples.ShuffleData(); // Премешивание
 
-            T[] datas = data.ToArray();
-            int[] marks = labels.ToArray();
+            T[] datas = dataSamples.ReturnDataset();
+            int[] marks = dataSamples.ReturnClasses();
 
             Vector[] features = new Vector[datas.Length];
 
@@ -105,7 +162,6 @@ namespace AI.DataPrepaire.Pipelines
             // Обучение классификатора
             Classifier.Train(features, marks);
         }
-
 
         /// <summary>
         /// Обучение и тестирование
@@ -143,5 +199,125 @@ namespace AI.DataPrepaire.Pipelines
 
             return MetricsForClassification.FullReport(Classify(testX), testY.ToArray(), isForEachClass:reportForEachClass);
         }
+
+        /// <summary>
+        /// Очистка данных
+        /// </summary>
+        public DatasetForClassifier ClearData(T[] dataIn, int[] classes) 
+        {
+            DatasetForClassifier dataSamples = new DatasetForClassifier(classes.Length/2);
+
+            for (int i = 0; i < classes.Length; i++)
+            {
+                // Добавляем элементы, если мы знаем их класс и они не являются аномальными и/или неподходящими
+                if (classes[i] != UnknowClass && !Detector.IsDetected(dataIn[i]))
+                    dataSamples.Add(dataIn[i], classes[i]);  
+            }
+
+            return dataSamples;
+        }
+
+
+
+
+
+        /// <summary>
+        /// Датасет для конвейера классификации
+        /// </summary>
+        [Serializable]
+        public class DatasetForClassifier : List<DataSample>
+        {
+
+            /// <summary>
+            /// Датасет для конвейера классификации
+            /// </summary>
+            public DatasetForClassifier() : base() { }
+
+            /// <summary>
+            /// Датасет для конвейера классификации
+            /// </summary>
+            /// <param name="cap">Емкость коллекции</param>
+            public DatasetForClassifier(int cap) : base(cap) { }
+
+            /// <summary>
+            /// Перемешать датасет
+            /// </summary>
+            public void ShuffleData() 
+            {
+                this.Shuffle();
+            }
+
+            /// <summary>
+            /// Вернуть данные
+            /// </summary>
+            public T[] ReturnDataset() 
+            {
+                var ret = new T[Count];
+
+                for (int i = 0; i < Count; i++)
+                    ret[i] = this[i].Obj;
+
+                return ret;
+            }
+
+            /// <summary>
+            /// Вернуть метки классов
+            /// </summary>
+            /// <returns></returns>
+            public int[] ReturnClasses() 
+            {
+                var ret = new int[Count];
+
+                for (int i = 0; i < Count; i++)
+                    ret[i] = this[i].ClassN;
+
+                return ret;
+            }
+
+            /// <summary>
+            /// Добавление объекта в выборку
+            /// </summary>
+            /// <param name="obj">Классифицируемый объект/param>
+            /// <param name="clIndex">Индекс</param>
+            public void Add(T obj, int clIndex)
+            {
+                Add(new DataSample(obj, clIndex));
+            }
+        }
+
+        /// <summary>
+        /// Элемент датасета
+        /// </summary>
+        [Serializable]
+        public class DataSample
+        {
+            /// <summary>
+            /// Индекс класса
+            /// </summary>
+            public int ClassN { get; set; }
+
+            /// <summary>
+            /// Классифицируемый объект
+            /// </summary>
+            public T Obj { get; set; }
+
+            /// <summary>
+            /// Элемент датасета
+            /// </summary>
+            public DataSample() { }
+
+            /// <summary>
+            /// Элемент датасета
+            /// </summary>
+            /// <param name="classN">Индекс класса</param>
+            /// <param name="obj">Классифицируемый объект</param>
+            public DataSample(T obj, int classN)
+            {
+                ClassN = classN;
+                Obj = obj;
+            }   
+        }
     }
+
+    
 }
---
+++ AI.DataPrepaire/Pipelines/Utils/IDetector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.DataPrepaire.Pipelines.Utils
+{
+    /// <summary>
+    /// Детектор событий/объектов
+    /// </summary>
+    public interface IDetector<T>
+    {
+        /// <summary>
+        /// Является ли данный объект элементом целевой группы
+        /// </summary>
+        bool IsDetected(T obj);
+    }
+}

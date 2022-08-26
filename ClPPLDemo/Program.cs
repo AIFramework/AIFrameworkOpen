b766128fc3a0846696d537d266b2496f7a1c1acc
Zahar A. Ponimash <ponimashz@mail.ru>
2022-08-26 12:52:44 +0300

    Демо классификации

---
+++ ClPPLDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using AI.DataPrepaire.DataNormalizers;
+using AI.DataPrepaire.FeatureExtractors;
+using AI.DataPrepaire.Pipelines;
+using AI.DataPrepaire.Pipelines.Utils;
+using AI.DataStructs.Algebraic;
+using AI.ML.Classifiers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClPPLDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем выборку
+            Random random = new Random(2);
+
+            //Класс 1
+            Vector cl1 = new Vector(1, 2, 9, 11, -2);
+            //Класс 2
+            Vector cl2 = new Vector(2, 0, 1, 13, -2);
+
+            List<Vector> xList = new List<Vector>();
+            List<int> yList = new List<int>();
+
+            //Выборка
+            for (int i = 0; i < 100; i++)
+            {
+                xList.Add(cl1 + 4*AI.Statistics.Statistic.RandNorm(5, random));
+                xList.Add(cl2 + 4*AI.Statistics.Statistic.RandNorm(5, random));
+                yList.Add(0);
+                yList.Add(1);
+            }
+
+            // Классификатор
+            Classifier classifier = new Classifier();
+            Console.WriteLine(classifier.TrainTest(xList.ToArray(), yList.ToArray()));
+        }
+    }
+
+    /// <summary>
+    /// Классификатор
+    /// </summary>
+    public class Classifier: ObjectClassifierPipeline<Vector> 
+    {
+        /// <summary>
+        /// Классификатор
+        /// </summary>
+        public Classifier() 
+        {
+            Normalizer = new ZNormalizer();
+            Detector = new NoDetector<Vector>();
+            Classifier = new KNNCl() { IsParsenMethod = true, K = 3 };
+            Extractor = new NoExtractor();
+        }
+    }
+}

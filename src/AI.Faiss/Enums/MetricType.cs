991c42a335713da5129af8df5692ccfaa6b08f43
potaninmt <marat.potanin12@mail.ru>
2024-02-03 22:24:14 +0300

    Faiss

---
+++ src/AI.Faiss/Enums/MetricType.cs
@@ -0,0 +1,21 @@
+﻿namespace AI.Faiss.Enums;
+
+/// <summary>
+/// Метрики расстояния из faiss. Данный тип индекса может не поддерживать все типы метрик.
+/// </summary>
+public enum MetricType
+{
+    METRIC_INNER_PRODUCT = 0, //< поиск максимального скалярного произведения
+    METRIC_L2 = 1,            //< поиск квадрата L2
+    METRIC_L1,                //< L1 (также известный как манхэттенское расстояние)
+    METRIC_Linf,              //< расстояние по бесконечной норме
+    METRIC_Lp,                //< расстояние L_p, p задается через faiss::Index
+                              // metric_arg
+
+    /// некоторые дополнительные метрики, определенные в scipy.spatial.distance
+    METRIC_Canberra = 20,
+    METRIC_BrayCurtis,
+    METRIC_JensenShannon,
+    METRIC_Jaccard, //< определяется как: sum_i(min(a_i, b_i)) / sum_i(max(a_i, b_i))
+                    //< где a_i, b_i > 0
+};
---
+++ src/AI.Faiss/Extensions/LinqExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AI.Faiss.Extensions;
+
+public static class LinqExtensions
+{
+    public static IEnumerable<T[]> Chunk<T>(this IEnumerable<T> source, int size)
+    {
+        if (size <= 0)
+            throw new ArgumentException("Chunk size must be greater than 0.", nameof(size));
+
+        using var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
+        {
+            yield return GetChunk(enumerator, size).ToArray();
+        }
+    }
+
+    private static IEnumerable<T> GetChunk<T>(IEnumerator<T> enumerator, int chunkSize)
+    {
+        do
+        {
+            yield return enumerator.Current;
+        } while (--chunkSize > 0 && enumerator.MoveNext());
+    }
+}
+

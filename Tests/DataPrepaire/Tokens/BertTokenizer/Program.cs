974492c64e8bf1ef4a19623ec806957dbf69ce5d
Zahar A. Ponimash <ponimashz@mail.ru>
2024-02-28 23:49:23 +0300

    Update Program.cs

--- Tests/DataPrepaire/Tokens/BertTokenizer/Program.cs
+++ Tests/DataPrepaire/Tokens/BertTokenizer/Program.cs
@@ -22,18 +22,20 @@ SimpleTest(embedder);
 static void SimpleTest(BertEmbedder embedder)
 {
 
+    for (int i = 0; i < 1000; i++)
+    {
+        // Векторизация предложений
+        Vector vects1 = embedder.ForwardSBert("Kittens love milk");
+        Vector vects2 = embedder.ForwardSBert("Visual Studio Code (VS Code) — текстовый редактор, разработанный Microsoft для Windows, Linux и macOS.");
+        Vector vects3 = embedder.ForwardSBert("Visual Studio Code, also commonly referred to as VS Code,[12] is a source-code editor developed by Microsoft for Windows, Linux and macOS.");
+        Vector vects1_rus = embedder.ForwardSBert("Котята любят молоко");
 
-    // Векторизация предложений
-    Vector vects1 = embedder.ForwardSBert("Kittens love milk");
-    Vector vects2 = embedder.ForwardSBert("Visual Studio Code (VS Code) — текстовый редактор, разработанный Microsoft для Windows, Linux и macOS.");
-    Vector vects3 = embedder.ForwardSBert("Visual Studio Code, also commonly referred to as VS Code,[12] is a source-code editor developed by Microsoft for Windows, Linux and macOS.");
-    Vector vects1_rus = embedder.ForwardSBert("Котята любят молоко");
-
-    // Рассчет близости между текстами
-    Console.WriteLine(vects1.Cos(vects1_rus));
-    Console.WriteLine(vects1.Cos(vects2));
-    Console.WriteLine(vects2.Cos(vects3));
-    Console.WriteLine(vects1.Cos(vects3));
+        // Рассчет близости между текстами
+        Console.WriteLine(vects1.Cos(vects1_rus));
+        Console.WriteLine(vects1.Cos(vects2));
+        Console.WriteLine(vects2.Cos(vects3));
+        Console.WriteLine(vects1.Cos(vects3));
+    }
     Console.ReadKey();
 }
 

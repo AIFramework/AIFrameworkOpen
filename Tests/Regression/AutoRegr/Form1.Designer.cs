2a019448efd9a1f0f04fdda842123c6bf2f4437b
Zahar A. Ponimash <ponimashz@mail.ru>
2025-07-09 17:21:18 +0300

    Тест (Авторегрессия)

---
+++ Tests/Regression/AutoRegr/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace AutoRegr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chartVisual1 = new AI.Charts.Control.ChartVisual();
+            this.SuspendLayout();
+            // 
+            // chartVisual1
+            // 
+            this.chartVisual1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chartVisual1.AutoScroll = true;
+            this.chartVisual1.BackColor = System.Drawing.Color.White;
+            this.chartVisual1.ChartName = "График";
+            this.chartVisual1.ForeColor = System.Drawing.Color.Black;
+            this.chartVisual1.IsContextMenu = true;
+            this.chartVisual1.IsLogScale = false;
+            this.chartVisual1.IsMoove = true;
+            this.chartVisual1.IsScale = true;
+            this.chartVisual1.IsShowXY = true;
+            this.chartVisual1.LabelX = "Ось Х";
+            this.chartVisual1.LabelY = "Ось Y";
+            this.chartVisual1.Location = new System.Drawing.Point(-1, 2);
+            this.chartVisual1.Name = "chartVisual1";
+            this.chartVisual1.Size = new System.Drawing.Size(1003, 520);
+            this.chartVisual1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 545);
+            this.Controls.Add(this.chartVisual1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AI.Charts.Control.ChartVisual chartVisual1;
+    }
+}
+
---
+++ Tests/Regression/AutoRegr/Form1.cs
@@ -0,0 +1,72 @@
+﻿using AI.DataStructs.Algebraic;
+using AI.ML.Regression;
+using AI.ML.SeqPredict;
+using AI.Statistics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace AutoRegr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            int wSize = 100;
+            int time = 600;
+            int timeNew = 600;
+            int fullTime = time + timeNew;
+
+
+            Random random = new Random();
+
+            Vector t = Vector.Seq(0, 1, time);
+            Vector x = t.Transform(r1 => Math.Sin(2 * 10 * r1 * Math.PI / time) + Math.Sin(2 * 13 * r1 * Math.PI / time) + Math.Cos(2 * 2 * r1 * Math.PI / time));
+            x = x.Minimax();
+            var xBinary = x.Transform(r1 => (int)(15 *  r1));
+
+
+            Vector t1 = Vector.Seq(0, 1, fullTime);
+            Vector xTest = t1.Transform(r1 => Math.Sin(2 * 10 * r1 * Math.PI / time) + Math.Sin(2 * 13 * r1 * Math.PI / time) + Math.Cos(2 * 2 * r1 * Math.PI / time));
+            xTest = xTest.Minimax();
+            var xBinaryTest = xTest.Transform(r1 => (int)(15 * r1));
+
+
+            var xNoise = xBinary.Transform(r1 => r1 + random.Next(-3, 4));
+            var ar = new AR(wSize);// SeqPrediction(reg, wSize);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            ar.Train(xNoise);
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds / 1000.0}");
+
+
+            var pred = ar.Predict(xNoise, timeNew);
+
+            var y_pred = new Vector(pred.GetRange(time, timeNew - 1));
+            var y = new Vector(xBinaryTest.GetRange(time, timeNew - 1));
+            var r = Statistic.CorrelationCoefficient(y_pred, y);
+
+            Console.WriteLine(r*r);
+
+            chartVisual1.AddPlot(t, xNoise, "Исходные данные");
+            chartVisual1.AddPlot(t1, xBinaryTest, "Сигнал без шума данные");
+            chartVisual1.AddPlotBlack(pred);
+
+            
+
+
+        }
+    }
+}

71815999e15096d7d928b388767d9e9721ed328a
Zahar A. Ponimash <ponimashz@mail.ru>
2025-10-08 05:13:44 +0300

    .

--- Tests/Calculator/Program.cs
+++ Tests/Calculator/Program.cs
@@ -14,41 +14,41 @@ public class Program
     public static void Main(string[] args)
     {
 
-        string script =
+//        string script =
 
-@"
-a = 10
-if (a > 2^5) {
-    b = a * 2
-}
-else
-{
-    v1 = [1,2,3]
-    v2 = [0.1,-0.2, 0.3]
-    b = dot(v1, v2)/(mag(v1)*mag(v2)) // косинус
-    b = b * 10
-}
-b // значение b
+//@"
+//a = 10
+//if (a > 2^5) {
+//    b = a * 2
+//}
+//else
+//{
+//    v1 = [1,2,3]
+//    v2 = [0.1,-0.2, 0.3]
+//    b = dot(v1, v2)/(mag(v1)*mag(v2)) // косинус
+//    b = b * 10
+//}
+//b // значение b
 
-// Факториал
-factorial = 1
+//// Факториал
+//factorial = 1
 
-for (j=1; j<=b; j=j+1) {
-    factorial = factorial * j
-}
+//for (j=1; j<=b; j=j+1) {
+//    factorial = factorial * j
+//}
 
-factorial // финальное значение
+//factorial // финальное значение
 
-fact(b) == factorial // проверка
-gamma(b) > factorial // проверка
-gamma(b)
+//fact(b) == factorial // проверка
+//gamma(b) > factorial // проверка
+//gamma(b)
 
-"
+//"
 
-;
-        Processor processor = new Processor();
-        var answer = processor.Run(script);
-        Console.WriteLine(string.Join("\n", answer));
+//;
+//        Processor processor = new Processor();
+//        var answer = processor.Run(script);
+//        Console.WriteLine(string.Join("\n", answer));
 
 
 

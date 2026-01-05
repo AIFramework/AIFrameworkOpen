b3b4728384ad894afa146df699e183b56ac30325
Zahar A. Ponimash <ponimashz@mail.ru>
2026-01-05 05:29:19 +0300

    .

--- Tests/Signals/SignalArray/Program.cs
+++ Tests/Signals/SignalArray/Program.cs
@@ -1,10 +1,11 @@
 ﻿using AI;
 using AI.DataStructs.Algebraic;
 using AI.DSP.Multiray;
+using SignalArray;
 using Environment = AI.DSP.Multiray.Environment;
 
 double[] coordD1 = [1, 3];
-double[] coordD2 = [1.4, 3];
+double[] coordD2 = [10.4, 3];
 double[] coordD3 = [1.2, 3.2];
 
 double[] coordS1 = [-5, -13];
@@ -13,17 +14,23 @@ Detector detector1 = new Detector(coordD1);
 Detector detector2 = new Detector(coordD2);
 Detector detector3 = new Detector(coordD3);
 
-Source source = new SinSource(1000, coordS1);
+Source source = new SinSource(10000, coordS1);
 
 Environment env = new Environment();
+
+
 env.Detectors.Add(detector1);
 env.Detectors.Add(detector2);
-env.Detectors.Add(detector3);
 env.Sources.Add(source);
 
 var signals = env.GetSignals();
-var corr = Matrix.GetCovMatrix(signals.ToArray());
-corr /= corr.Std();
 
-Console.WriteLine(corr);
+var r1r2 = TwoMicro.GetR1R2Correlation(signals[0], signals[1], env.SR, env.WaveSpeed);
+var r1r2F = TwoMicro.GetR1R2FFT(signals[0], signals[1], env.SR, env.WaveSpeed);
+
+var r1Real = Environment.GetDist(detector1, source);
+var r2Real = Environment.GetDist(detector2, source);
+var dtReal = Environment.GetDeltaT(detector1, detector2, source, env.WaveSpeed);
+
+Console.WriteLine();
 Console.WriteLine();
---
+++ Tests/Signals/SignalArray/TwoMicro.cs
@@ -0,0 +1,81 @@
+﻿using AI;
+using AI.DataStructs.Algebraic;
+using AI.DataStructs.WithComplexElements;
+using AI.DSP.DSPCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalArray;
+
+public class TwoMicro
+{
+    public static Tuple<double, double> GetR1R2FFT(Vector sig1, Vector sig2, double sr, double v)
+    {
+        ComplexVector Sp1 = FFT.CalcFFT(sig1);
+        ComplexVector Sp2 = FFT.CalcFFT(sig2);
+
+        Vector a1 = Sp1.MagnitudeVector, a2 = Sp2.MagnitudeVector;
+        int idx = (a1 + a2).MaxElementIndex();
+
+        if (idx == 0) idx = 1;
+
+        double phase1 = Math.Atan2(Sp1[idx].Imaginary, Sp1[idx].Real);
+        double phase2 = Math.Atan2(Sp2[idx].Imaginary, Sp2[idx].Real);
+        double df = phase2 - phase1;
+
+        if (df > Math.PI) df -= 2 * Math.PI;
+        if (df < -Math.PI) df += 2 * Math.PI;
+
+        double dt = Sp1.Count * df / (sr * idx * 2 * Math.PI);
+        double dr = Math.Abs(dt * v);
+
+        double ratioR_sq = (sig1.Max() * sig1.Max()) / (sig2.Max() * sig2.Max());
+
+        double r1, r2;
+
+        if (ratioR_sq > 1)
+        {
+            r1 = dr / (Math.Sqrt(ratioR_sq) - 1);
+            r2 = r1 * Math.Sqrt(ratioR_sq);
+        }
+        else
+        {
+            ratioR_sq = 1 / ratioR_sq;
+            r2 = dr / (Math.Sqrt(ratioR_sq) - 1);
+            r1 = r2 * Math.Sqrt(ratioR_sq);
+        }
+
+        return Tuple.Create(r1, r2);
+    }
+
+
+    public static Tuple<double, double> GetR1R2Correlation(Vector sig1, Vector sig2, double sr, double v)
+    {
+        var corr = Correlation.CrossCorrelation(sig1, sig2);
+        int centerIndex = sig2.Count - 1;
+        int maxIndex = corr.MaxElementIndex();
+        double dt = (maxIndex - centerIndex) / sr;
+        double dr = Math.Abs(dt * v);
+
+        double ratioR_sq = (sig1.Max() * sig1.Max()) / (sig2.Max() * sig2.Max());
+
+        double r1, r2;
+
+        if (ratioR_sq > 1)
+        {
+            r1 = dr / (Math.Sqrt(ratioR_sq) - 1);
+            r2 = r1 * Math.Sqrt(ratioR_sq);
+        }
+        else
+        {
+            ratioR_sq = 1 / ratioR_sq;
+            r2 = dr / (Math.Sqrt(ratioR_sq) - 1);
+            r1 = r2 * Math.Sqrt(ratioR_sq);
+        }
+
+        return Tuple.Create(r1, r2);
+    }
+}
